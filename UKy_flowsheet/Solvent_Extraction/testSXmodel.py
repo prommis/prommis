82957a03674cb84124d1caba3680859279bc6b81
MarcusHolly <marcus.holly@keylogic.com>
2023-09-07 09:00:25 -0400

    Initial flowsheet from leaching to SX

---
+++ UKy_flowsheet/Solvent_Extraction/testSXmodel.py
@@ -0,0 +1,95 @@
+from pyomo.environ import (
+    ConcreteModel,
+    SolverFactory, 
+)
+
+from idaes.core import FlowsheetBlock
+
+from idaes.core.util.model_statistics import degrees_of_freedom as dof
+
+from workspace.UKy_flowsheet.Solvent_Extraction.REESXmodel import REESX
+from workspace.UKy_flowsheet.Solvent_Extraction.REEAqdistribution import REESolExAqParameters
+from workspace.UKy_flowsheet.Solvent_Extraction.REEOgdistribution import REESolExOgParameters
+
+from pyomo.util.check_units import assert_units_consistent
+
+m = ConcreteModel()
+m.fs = FlowsheetBlock(dynamic=False)
+m.fs.prop_a = REESolExAqParameters()
+m.fs.prop_o = REESolExOgParameters()
+
+m.fs.solex = REESX(number_of_finite_elements=3,
+                       aqueous_streams = {"Acidsoln":{"property_package":m.fs.prop_a, "flow_direction":1}},
+                       organic_streams = {"Orgacid":{"property_package":m.fs.prop_o, "flow_direction":2}})
+
+
+#Aqueous feed fixing
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Al"].fix(0)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Ca"].fix(0.02)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Fe"].fix(0)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Si"].fix(0)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Sc"].fix(0.92)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Y"].fix(2.82)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["La"].fix(8.98)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Ce"].fix(19.94)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Pr"].fix(3.34)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Nd"].fix(9.04)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Pm"].fix(0)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Sm"].fix(1.63)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Eu"].fix(0.11)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Gd"].fix(0.77)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Tb"].fix(0.33)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Dy"].fix(0.45)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Ho"].fix(0)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Er"].fix(0)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Tm"].fix(0.18)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Yb"].fix(0.29)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Lu"].fix(0.14)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["Th"].fix(0)
+m.fs.solex.Acidsoln_inlet_state[0].flow_mass["U"].fix(0)
+
+m.fs.solex.Acidsoln_inlet_state[0].flow_vol.fix(4.4)
+
+#Organic feed fixing
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Al"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Ca"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Fe"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Si"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Sc"].fix(19.93)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Y"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["La"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Ce"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Pr"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Nd"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Pm"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Sm"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Eu"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Gd"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Tb"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Dy"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Ho"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Er"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Tm"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Yb"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Lu"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["Th"].fix(0)
+m.fs.solex.Orgacid_inlet_state[0].flow_mass["U"].fix(0)
+
+m.fs.solex.Orgacid_inlet_state[0].flow_vol.fix(62.01)
+
+print(dof(m))
+
+assert_units_consistent(m)
+
+solver = SolverFactory("ipopt")
+solver.solve(m, tee=True)
+
+# All organic outlets display
+m.fs.solex.Orgacid[0,1].flow_mass.display()
+m.fs.solex.Orgacid[0,2].flow_mass.display()
+m.fs.solex.Orgacid[0,3].flow_mass.display()
+
+# All aqueous outlets display
+m.fs.solex.Acidsoln[0,1].flow_mass.display()
+m.fs.solex.Acidsoln[0,2].flow_mass.display()
+m.fs.solex.Acidsoln[0,3].flow_mass.display()
---
+++ UKy_flowsheet/leaching/leach_reactions.py
@@ -0,0 +1,265 @@
+#################################################################################
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
+# Design of Advanced Energy Systems (IDAES).
+#
+# Copyright (c) 2018-2023 by the software owners: The Regents of the
+# University of California, through Lawrence Berkeley National Laboratory,
+# National Technology & Engineering Solutions of Sandia, LLC, Carnegie Mellon
+# University, West Virginia University Research Corporation, et al.
+# All rights reserved.  Please see the files COPYRIGHT.md and LICENSE.md
+# for full copyright and license information.
+#################################################################################
+"""
+Simple leaching model for West Kentucky No. 13 coal refuse in H2SO4.
+
+Authors: Andrew Lee
+"""
+from pyomo.environ import Constraint, Expression, Param, Set, units, Var
+from pyomo.common.config import ConfigValue
+
+from idaes.core import (
+    declare_process_block_class,
+    ProcessBlockData,
+    ProcessBlock,
+)
+from idaes.core.base import property_meta
+from idaes.core.util.misc import add_object_reference
+
+
+# -----------------------------------------------------------------------------
+# Leach solution property package
+@declare_process_block_class("CoalRefuseLeachingReactions")
+class CoalRefuseLeachingReactionsData(ProcessBlockData, property_meta.HasPropertyClassMetadata):
+    def build(self):
+        super().build()
+
+        self._reaction_block_class = CoalRefuseLeachingReactionsBlock
+
+        self.reaction_idx = Set(
+            initialize=[
+                "Al2O3",
+                "Fe2O3",
+                "CaO",
+                "Sc2O3",
+                "Y2O3",
+                "La2O3",
+                "Ce2O3",
+                "Pr2O3",
+                "Nd2O3",
+                "Sm2O3",
+                "Gd2O3",
+                "Dy2O3",
+            ]
+        )
+
+        self.reaction_stoichiometry = {
+            ("Al2O3", "liquid", "Al"): 2,
+            ("Al2O3", "liquid", "H2O"): 3,
+            ("Al2O3", "solid", "Al2O3"): -1,
+            ("Al2O3", "liquid", "H"): -6,
+            ("Fe2O3", "liquid", "Fe"): 2,
+            ("Fe2O3", "liquid", "H2O"): 3,
+            ("Fe2O3", "solid", "Fe2O3"): -1,
+            ("Fe2O3", "liquid", "H"): -6,
+            ("CaO", "liquid", "Ca"): 1,
+            ("CaO", "liquid", "H2O"): 1,
+            ("CaO", "solid", "CaO"): -1,
+            ("CaO", "liquid", "H"): -2,
+            ("Sc2O3", "liquid", "Sc"): 2,
+            ("Sc2O3", "liquid", "H2O"): 3,
+            ("Sc2O3", "solid", "Sc2O3"): -1,
+            ("Sc2O3", "liquid", "H"): -6,
+            ("Y2O3", "liquid", "Y"): 2,
+            ("Y2O3", "liquid", "H2O"): 3,
+            ("Y2O3", "solid", "Y2O3"): -1,
+            ("Y2O3", "liquid", "H"): -6,
+            ("La2O3", "liquid", "La"): 2,
+            ("La2O3", "liquid", "H2O"): 3,
+            ("La2O3", "solid", "La2O3"): -1,
+            ("La2O3", "liquid", "H"): -6,
+            ("Ce2O3", "liquid", "Ce"): 2,
+            ("Ce2O3", "liquid", "H2O"): 3,
+            ("Ce2O3", "solid", "Ce2O3"): -1,
+            ("Ce2O3", "liquid", "H"): -6,
+            ("Pr2O3", "liquid", "Pr"): 2,
+            ("Pr2O3", "liquid", "H2O"): 3,
+            ("Pr2O3", "solid", "Pr2O3"): -1,
+            ("Pr2O3", "liquid", "H"): -6,
+            ("Nd2O3", "liquid", "Nd"): 2,
+            ("Nd2O3", "liquid", "H2O"): 3,
+            ("Nd2O3", "solid", "Nd2O3"): -1,
+            ("Nd2O3", "liquid", "H"): -6,
+            ("Sm2O3", "liquid", "Sm"): 2,
+            ("Sm2O3", "liquid", "H2O"): 3,
+            ("Sm2O3", "solid", "Sm2O3"): -1,
+            ("Sm2O3", "liquid", "H"): -6,
+            ("Gd2O3", "liquid", "Gd"): 2,
+            ("Gd2O3", "liquid", "H2O"): 3,
+            ("Gd2O3", "solid", "Gd2O3"): -1,
+            ("Gd2O3", "liquid", "H"): -6,
+            ("Dy2O3", "liquid", "Dy"): 2,
+            ("Dy2O3", "liquid", "H2O"): 3,
+            ("Dy2O3", "solid", "Dy2O3"): -1,
+            ("Dy2O3", "liquid", "H"): -6,
+        }
+
+        self.A = Param(
+            self.reaction_idx,
+            initialize={
+                "Al2O3": 503.5875248,
+                "Fe2O3": 322.6139099,
+                "CaO": 109.3157001,
+                "Sc2O3": 0.04725527,
+                "Y2O3": 0.079996528,
+                "La2O3": 0.202377027,
+                "Ce2O3": 0.617265745,
+                "Pr2O3": 0.054514632,
+                "Nd2O3": 0.288997052,
+                "Sm2O3": 0.031614808,
+                "Gd2O3": 0.072328712,
+                "Dy2O3": 0.00949131,
+            },
+            units=units.L/units.mol/units.hour,
+            mutable=True,
+        )
+
+        self.B = Param(
+            self.reaction_idx,
+            initialize={
+                "Al2O3": 0.0021735,
+                "Fe2O3": 0.026437074,
+                "CaO": 0.110283447,
+                "Sc2O3": 0.009447767,
+                "Y2O3": 0.028245478,
+                "La2O3": 0.082176004,
+                "Ce2O3": 0.084370281,
+                "Pr2O3": 0.112379996,
+                "Nd2O3": 0.09284766,
+                "Sm2O3": 0.045429147,
+                "Gd2O3": 0.10010438,
+                "Dy2O3": 0.052401701,
+            },
+            units=units.hour**-1,
+            mutable=True,
+        )
+
+    @classmethod
+    def define_metadata(cls, obj):
+        obj.add_default_units(
+            {
+                "time": units.hour,
+                "length": units.m,
+                "mass": units.kg,
+                "amount": units.mol,
+                "temperature": units.K,
+            }
+        )
+
+    @property
+    def reaction_block_class(self):
+        if self._reaction_block_class is not None:
+            return self._reaction_block_class
+        else:
+            raise AttributeError(
+                "{} has not assigned a ReactionBlock class to be associated "
+                "with this reaction package. Please contact the developer of "
+                "the reaction package.".format(self.name)
+            )
+
+    def build_reaction_block(self, *args, **kwargs):
+        """
+        Methods to construct a ReactionBlock associated with this
+        ReactionParameterBlock. This will automatically set the parameters
+        construction argument for the ReactionBlock.
+
+        Returns:
+            ReactionBlock
+
+        """
+        default = kwargs.pop("default", {})
+        initialize = kwargs.pop("initialize", {})
+
+        if initialize == {}:
+            default["parameters"] = self
+        else:
+            for i in initialize.keys():
+                initialize[i]["parameters"] = self
+
+        return self.reaction_block_class(  # pylint: disable=not-callable
+            *args, **kwargs, **default, initialize=initialize
+        )
+
+
+class _CoalRefuseLeachingReactionsBlock(ProcessBlock):
+    pass
+
+
+@declare_process_block_class(
+    "CoalRefuseLeachingReactionsBlock", block_class=_CoalRefuseLeachingReactionsBlock
+)
+class CoalRefuseLeachingReactionsData(ProcessBlockData):
+    # Create Class ConfigBlock
+    CONFIG = ProcessBlockData.CONFIG()
+    CONFIG.declare(
+        "parameters",
+        ConfigValue(
+            # TODO
+            # domain=is_reaction_parameter_block,
+            description="""A reference to an instance of the Heterogeneous Reaction Parameter
+    Block associated with this property package.""",
+        ),
+    )
+
+    def build(self):
+        super().build()
+
+        add_object_reference(self, "_params", self.config.parameters)
+
+        self.reaction_rate = Var(
+            self.params.reaction_idx,
+            initialize=0,
+            units=units.mol/units.litre/units.hour
+        )
+
+        # TODO
+        l_block = self.parent_block().liquid[self.index()]
+        s_block = self.parent_block().solid[self.index()]
+
+        h_conc = l_block.conc_mole_acid["H"]
+
+        def rule_c_max(b, j):
+            if j in ["H2O", "H", "HSO4", "SO4"]:
+                return Expression.Skip
+
+            if j == "Ca":
+                m = "CaO"
+                n = 1
+            else:
+                m = j+"2O3"
+                n = 2
+            return (
+                units.convert(
+                    l_block.conc_mass_metals[j] / l_block.params.mw[j],
+                    to_units=units.mol/units.litre,
+                ) +
+                n*s_block.flow_mass*s_block.mass_frac_comp[m]/l_block.flow_vol/s_block.params.mw[m]
+            )
+        self.c_max = Expression(l_block.component_list, rule=rule_c_max)
+
+        def rule_reaction_rate_eq(b, r):
+            if r.startswith("Y"):
+                j = "Y"
+            else:
+                j = r[:2]
+
+            return b.reaction_rate[r] == units.convert(
+                b.params.A[r]*h_conc**2 + b.params.B[r]*self.c_max[j],
+                to_units=units.mol/units.litre/units.hour,
+            )
+
+        self.reaction_rate_eq = Constraint(self.params.reaction_idx, rule=rule_reaction_rate_eq)
+
+    @property
+    def params(self):
+        return self._params

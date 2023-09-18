68c2241b9b808e0635b9755abccd93252e47afc3
MarcusHolly <marcus.holly@keylogic.com>
2023-09-18 14:01:32 -0400

    Incorporate JinLiang's updates to roaster model

--- roasting/oxalate_roaster_flowsheet.py
+++ roasting/oxalate_roaster_flowsheet.py
@@ -40,7 +40,10 @@ from idaes.models_extra.power_generation.properties.natural_gas_PR import (
     EosType,
 )
 
+import sys
 
+sys.path.append('../precipitate')
+from precip_prop import PrecipitateStateParameterBlock
 
 _log = idaeslog.getModelLogger(__name__)
 
@@ -55,10 +58,26 @@ def main(m=None):
         # Add a flowsheet object to the model
         m.fs = FlowsheetBlock(dynamic=False)
         # Add property packages to flowsheet library
-        gas_species={"O2", "H2O", "CO2", "N2"}
+        gas_species = {"O2", "H2O", "CO2", "N2"}
         m.fs.prop_gas = GenericParameterBlock(
             **get_prop(gas_species, ["Vap"], EosType.IDEAL),
             doc="gas property",
+        )
+        key_components = {
+            "H^+",
+            "Ce^3+",
+            "Al^3+",
+            "Fe^3+",
+            # "Fe^2+",
+            # "Ca^2+",
+            # "Mg^2+",
+            "C2O4^2-",
+            # "NO3^-",
+            # "SO4^2-",
+            # "Cl^-",
+        }
+        m.fs.prop_solid = PrecipitateStateParameterBlock(
+            key_components=key_components,
         )
 
     create_model(m)
@@ -68,22 +87,23 @@ def main(m=None):
     print('dof=', dof)
     result = solver.solve(m, tee=True)
     print('Gas feed mole flow =', pyo.value(m.fs.roaster.gas_in[0].flow_mol), 'mol/s')
-    print('Solid feed mass flow =', m.fs.roaster.flow_mas_feed[0].value, 'kg/s')
+    print('Solid feed Ce mole flow =', m.fs.roaster.solid_in[0].flow_mol_comp['Ce2(C2O4)3(s)'].value, 'mol/s')
     print('heat_duty=', m.fs.roaster.heat_duty[0].value)
     print('mass fraction of metal oxide in solid product:')
     for x in m.fs.roaster.metal_list:
-        print(x, pyo.value(m.fs.roaster.mass_frac_comp_product[0,x]))
+        print(x, pyo.value(m.fs.roaster.mass_frac_comp_product[0, x]))
     return m
 
 
 def create_model(m):
     """Create unit models"""
     m.fs.roaster = REEOxalateRoaster(
-        property_package=m.fs.prop_gas,
+        property_package_gas=m.fs.prop_gas,
+        property_package_precipitate=m.fs.prop_solid,
         has_holdup=False,
         has_heat_transfer=True,
         has_pressure_change=True,
-        metal_list = ["Sc","Y","La","Ce","Pr","Nd","Sm","Eu","Gd","Tb","Dy","Tm","Yb","Lu"]
+        # metal_list = ["Sc","Y","La","Ce","Pr","Nd","Sm","Eu","Gd","Tb","Dy","Tm","Yb","Lu"], default is ["Ce"] only
     )
 
 
@@ -96,10 +116,10 @@ def set_inputs(m):
     fgas = 0.00781
     # inlet flue gas composition, typical flue gas by buring CH4 with air with stoichiometric ratio 0f 2.3
     gas_comp = {
-    "O2":  0.1118,
-    "H2O": 0.1005,
-    "CO2": 0.0431,
-    "N2":  0.7446,
+        "O2": 0.1118,
+        "H2O": 0.1005,
+        "CO2": 0.0431,
+        "N2": 0.7446,
     }
     for i, v in gas_comp.items():
         m.fs.roaster.gas_inlet.mole_frac_comp[0, i].fix(v)
@@ -110,13 +130,12 @@ def set_inputs(m):
     m.fs.roaster.gas_outlet.temperature.fix(873.15)
 
     # solid feed temperature
-    m.fs.roaster.temp_feed.fix(298.15)
+    m.fs.roaster.solid_in[0].temperature.fix(298.15)
+    m.fs.roaster.solid_in[0].flow_mol_comp['Ce2(C2O4)3(s)'].fix(6.1e-5)
+    m.fs.roaster.flow_mol_moist_feed.fix(6.75e-4)
     # total solid mass flow rate including surface moisture
-    m.fs.roaster.flow_mas_feed.fix((0.12+0.04372)/3600)
-    
-    # mass fraction of moisture
-    m.fs.roaster.mass_frac_moist_feed.fix(1-0.266)
-    
+
+    '''
     m.fs.roaster.mass_frac_feed_dry[0,'Sc'].fix(0.001648997)
     m.fs.roaster.mass_frac_feed_dry[0,'Y'].fix(0.0619823)
     m.fs.roaster.mass_frac_feed_dry[0,'La'].fix(0.160501197)
@@ -131,8 +150,9 @@ def set_inputs(m):
     m.fs.roaster.mass_frac_feed_dry[0,'Tm'].fix(0.004159939)
     m.fs.roaster.mass_frac_feed_dry[0,'Yb'].fix(0.005879861)
     m.fs.roaster.mass_frac_feed_dry[0,'Lu'].fix(0.002971853)
-
+    '''
     m.fs.roaster.frac_comp_recovery.fix(0.95)
+
 
 if __name__ == "__main__":
     """
@@ -141,6 +161,3 @@ if __name__ == "__main__":
     to run dynamic model, call main_dyn()
     """
     m = main()
-    
-
-

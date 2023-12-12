b6af0bef128e491ccb5857671ca7fec931fa2275
Ludovico Bianchi <lbianchi@lbl.gov>
2023-12-12 10:01:54 -0600

    Set pytest import mode to importlib and fix incompatible imports

--- src/prommis/Solvent_Extraction/test_SXmodel.py
+++ src/prommis/Solvent_Extraction/test_SXmodel.py
@@ -10,9 +10,9 @@ from idaes.core.initialization.block_triangularization import (
 )
 from idaes.core.initialization import InitializationStatus
 
-from .SolventExtraction import SolventExtraction
-from .REEAqdistribution import REESolExAqParameters
-from .REEOgdistribution import REESolExOgParameters
+from prommis.Solvent_Extraction.SolventExtraction import SolventExtraction
+from prommis.Solvent_Extraction.REEAqdistribution import REESolExAqParameters
+from prommis.Solvent_Extraction.REEOgdistribution import REESolExOgParameters
 
 solver = get_solver()
 

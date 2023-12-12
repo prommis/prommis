15f6ebaca280c10a13f9c84904a42dd30c9506d2
Ludovico Bianchi <lbianchi@lbl.gov>
2023-12-12 10:02:57 -0600

    Register pytest markers

---
+++ src/prommis/conftest.py
@@ -0,0 +1,16 @@
+from pytest import Config
+
+
+IDAES_MARKERS = {
+    "unit": "Quick tests that do not require a solver, must run in < 2 s",
+    "component": "Quick tests that may require a solver",
+    "integration": "Long duration tests",
+}
+
+
+def pytest_configure(config: Config):
+
+    for spec, descr in IDAES_MARKERS.items():
+        config.addinivalue_line(
+            "markers", f"{spec}: {descr}"
+        )

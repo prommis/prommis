bf87ae64a2d69ab2c53e980ef483b394ede911a1
MarcusHolly <marcus.holly@keylogic.com>
2023-12-14 09:30:17 -0500

    Remove instances of known_issue pymarks that have been resolved

--- src/prommis/precipitate/tests/test_precipitator.py
+++ src/prommis/precipitate/tests/test_precipitator.py
@@ -84,7 +84,6 @@ class TestPrec(object):
 
         return m
 
-    @pytest.mark.known_issue(6)
     @pytest.mark.build
     @pytest.mark.unit
     def test_build(self, prec):
@@ -133,7 +132,6 @@ class TestPrec(object):
         initializer.initialize(prec.fs.unit)
         assert initializer.summary[prec.fs.unit]["status"] == InitializationStatus.Ok
 
-    @pytest.mark.known_issue(6)
     @pytest.mark.component
     def test_var_scaling(self, prec):
         unscaled_var_list = list(

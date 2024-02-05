b5f73282a990d87c28b1b204edeac1f24719010e
Dan Gunter <dkgunter@lbl.gov>
2024-02-05 15:50:26 -0800

    basically correct fsapi, just need to fix what's actually exported

--- src/prommis/uky/uky_flowsheet_ui.py
+++ src/prommis/uky/uky_flowsheet_ui.py
@@ -17,8 +17,11 @@ Authors: Dan Gunter (LBNL)
 __author__ = "Dan Gunter"
 
 # stdlib
+import logging
 # third party
-from watertap.ui.fsapi import FlowsheetInterface  # pylint: disable=import-error
+from idaes import logger as idaeslog
+from watertap.ui.fsapi import FlowsheetInterface
+from watertap.ui.fsapi import FlowsheetCategory as FC
 
 # package
 from prommis.uky.uky_flowsheet import (
@@ -26,7 +29,10 @@ from prommis.uky.uky_flowsheet import (
     set_operating_conditions,
     set_scaling,
     solve,
+    initialize_system
 )
+
+_log = idaeslog.getLogger(__name__)
 
 
 def export_to_ui():
@@ -37,21 +43,79 @@ def export_to_ui():
         do_solve=solve_flowsheet,
         get_diagram=get_diagram,
         requires_idaes_solver=True,
-        category="PrOMMiS",
+        category=FC.wastewater,
         build_options={},
     )
 
 
 def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs):
+    wlog = logging.getLogger("idaes.watertap.ui.fsapi")
+    wlog.setLevel(logging.DEBUG)
+
     exports.from_csv(file="uky_flowsheet_ui.csv", flowsheet=flowsheet)
+    fs = flowsheet
+
+    # TODO: Fix compositions to match test_uky_flowsheet exactly
+
+    compositions = {
+        "liquid1": ("'Al', 'Ca', 'Ce', 'Dy', 'Fe', 'Gd', 'H', 'H2O', 'HSO4', 'SO4', 'La', 'Nd', 'Pr',
+        'Sc', 'Sm', 'Y'),
+        "solex"
+    }
+    units = {
+        "conc_mass_comp": {
+            "outlets": [
+                ("leach liquid", "fs.leach.liquid_outlet", liquid1),
+            "solex rougher mscontactor organic":
+                "fs.solex_rougher.mscontactor.organic_outlet",
+            "solex rougher mscontactor aqueous":
+                "fs.solex_rougher.mscontactor.aqueous_outlet",
+            "solex cleaner mscontactor organic":
+                "fs.solex_cleaner.mscontactor.organic_outlet",
+            "solex cleaner mscontactor aqueous":
+                "fs.solex_cleaner.mscontactor.aqueous_outlet",
+            "precipitator cv aqueous":
+                "fs.precipitator.cv_aqueous.properties_out[0]",
+            ],
+            "value_name": "mass composition concentration"
+        },
+        "flow_mol_comp": {
+            "value_name": "flow molar composition",
+            "compositions": ("Al2(C2O4)3(s)", "Dy2(C2O4)3(s)", "Fe2(C2O4)3(s)",
+                             "Gd2(C2O4)3(s)", "La2(C2O4)3(s)", "Nd2(C2O4)3(s)",
+                             "Pr2(C2O4)3(s)", "Sc2(C2O4)3(s)", "Sm2(C2O4)3(s)",
+                             "Y2(C2O4)3(s)"),
+            "outlets": {
+                "precipitator precipitate": fs.precipitator.precipitate_outlet
+            }
+        }
+    }
+    # add exports for: {type of value} X {unit} X {composition}
+    for value_type, data in units.items():
+        for name, outlet in data["outlets"].items():
+            for comp in data["compositions"]:
+                obj_name = f"{outlet}.{value_type}[0,'{comp}']"
+                _log.debug(f"start: add UI export for obj={obj_name}")
+                exports.add(
+                    deferred_obj=obj_name,
+                    name=name,
+                    rounding=2,
+                    description=f"{name} outlet",
+                    is_input=False,
+                    is_output=True,
+                    output_category=data["value_name"]
+                )
+                _log.info(f"end: add UI export for obj={obj_name}")
 
 
 def build_flowsheet(build_options=None, **kwargs):
     m = build()
     set_operating_conditions(m)
     scaled_model = set_scaling(m)
-    # initialize_system(scaled_model) XXX: Currently not working
-    return m
+    # assert_units_consistent(scaled_model)
+    # assert degrees_of_freedom(scaled_model) == 0
+    initialize_system(scaled_model)
+    return scaled_model
 
 
 def get_diagram(build_options):
@@ -59,7 +123,7 @@ def get_diagram(build_options):
 
 
 def solve_flowsheet(flowsheet=None):
-    return solve(flowsheet)  # XXX: also not working
+    return solve(flowsheet)
 
 
 # for terminal debugging

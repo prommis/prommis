749c2acfb3a06435d7d334b8cbd74b246305b124
Dan Gunter <dkgunter@lbl.gov>
2024-02-06 10:41:23 -0800

    added import guard for testing

--- src/prommis/uky/tests/test_uky_flowsheet_ui.py
+++ src/prommis/uky/tests/test_uky_flowsheet_ui.py
@@ -1,18 +1,20 @@
 import pytest
-from prommis.uky import uky_flowsheet_ui as ui
+ui = pytest.importorskip(
+    "prommis.uky.uky_flowsheet_ui", reason="Flowsheet UI components not available"
+)
 
 
 @pytest.mark.unit
-def test_export():
+def test_uky_ui_export():
     interface = ui.export_to_ui()
 
 
 @pytest.mark.component
-def test_build():
+def test_uky_ui_build():
     ui.build_flowsheet()
 
 
 @pytest.mark.component
-def test_solve():
+def test_uky_ui_solve():
     flowsheet = ui.build_flowsheet()
     ui.solve_flowsheet(flowsheet)

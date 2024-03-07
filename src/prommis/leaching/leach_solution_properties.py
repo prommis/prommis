c4c84c12b7d71a1ebae8d8be28b777d38ec39df7
MarcusHolly <marcus.holly@keylogic.com>
2024-03-07 10:26:37 -0500

    Add Cl to leach aqueous prop pack

--- src/prommis/leaching/leach_solution_properties.py
+++ src/prommis/leaching/leach_solution_properties.py
@@ -47,6 +47,7 @@ class LeachSolutionParameterData(PhysicalParameterBlock):
         self.H = Component()
         self.HSO4 = Component()
         self.SO4 = Component()
+        self.Cl = Component()
 
         # REEs
         self.Sc = Component()
@@ -72,6 +73,7 @@ class LeachSolutionParameterData(PhysicalParameterBlock):
                 "H": 1e-3,
                 "HSO4": 97e-3,
                 "SO4": 96e-3,
+                "Cl": 35.453e-3,
                 "Sc": 44.946e-3,
                 "Y": 88.905e-3,
                 "La": 138.905e-3,

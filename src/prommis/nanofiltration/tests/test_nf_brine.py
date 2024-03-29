8858bbcf13e9f0f63ea1f5d25f5724f9233c065f
Molly Dougher <mdoughe6@nd.edu>
2024-03-29 13:04:38 -0400

    Updated the solution values in nanofiltration test

--- src/prommis/nanofiltration/tests/test_nf_brine.py
+++ src/prommis/nanofiltration/tests/test_nf_brine.py
@@ -22,25 +22,25 @@ def test_main():
     """
     m = main()
     test_dict = {
-        "pressure": [m.fs.pump.outlet.pressure[0] * 1e-5, 11.362],
-        "area": [m.fs.unit.area, 999.99],
+        "pressure": [m.fs.pump.outlet.pressure[0] * 1e-5, 11.362344380368109],
+        "area": [m.fs.unit.area, 999.9990553265868],
         "nf_recovery": [
             m.fs.unit.recovery_vol_phase[0.0, "Liq"] * 100,
-            94.999,
+            94.9999989334488,
         ],
         "li_rejection": [
             m.fs.unit.rejection_intrinsic_phase_comp[0, "Liq", "Li_+"].value * 100,
-            1.838,
+            1.8379272407186817,
         ],
         "feed_ion_ratio": [
             (m.fs.feed.flow_mol_phase_comp[0, "Liq", "Mg_2+"].value / 0.024)
             / (m.fs.feed.flow_mol_phase_comp[0, "Liq", "Li_+"].value / 0.0069),
-            0.5077,
+            0.5077446970643547,
         ],
         "perm_ion_ratio": [
             (m.fs.permeate.flow_mol_phase_comp[0, "Liq", "Mg_2+"].value / 0.024)
             / (m.fs.permeate.flow_mol_phase_comp[0, "Liq", "Li_+"].value / 0.0069),
-            0.497,
+            0.4972950260825848,
         ],
     }
     for model_result, testval in test_dict.values():

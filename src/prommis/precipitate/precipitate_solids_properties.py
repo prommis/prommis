88d17dc9a8b07bfa633c372d022f1d66a251b970
agarciadiego <40575271+agarciadiego@users.noreply.github.com>
2024-04-30 13:09:18 -0400

    Clean up precipitator and documentation (#62)
    * cleaning precipitator plus doc
    
    * black
    
    * renaming equation in test
    
    * cleaning tutorial
    
    * running black on tutorial
    
    * changing kernel name
    
    * changing kernel
    
    * Update src/prommis/precipitate/precipitator.py
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    
    * fixing typos and eq. and adding python modules
    
    * fixing word solids and adding leach train
    
    * adding indents to list
    
    * Update src/prommis/precipitate/precipitator.py
    
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>
    
    * Update src/prommis/precipitate/precipitator.py
    
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>
    
    * Update src/prommis/precipitate/precipitator.py
    
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>
    
    * Update src/prommis/precipitate/precipitator.py
    
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>
    
    * first try on fix bullet list
    
    * removing extra cell
    
    ---------
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>

--- src/prommis/precipitate/precipitate_solids_properties.py
+++ src/prommis/precipitate/precipitate_solids_properties.py
@@ -11,7 +11,7 @@
 # for full copyright and license information.
 #################################################################################
 """
-Initial property package for West Kentucky No. 13 coal refuse.
+Preliminary property package for West Kentucky No. 13 coal refuse.
 
 Authors: Alejandro Garciadiego
 """
@@ -48,6 +48,23 @@ def _config_blk_build(blk):
 
 @declare_process_block_class("PrecipitateParameters")
 class PrecipitateParametersData(PhysicalParameterBlock):
+    """
+    Solid phase property package for oxalate precipitation.
+
+    Based on assay provided in:
+
+    RESEARCH PERFORMANCE FINAL REPORT, Pilot-Scale Testing of an Integrated
+    Circuit for the Extraction of Rare Earth Minerals and Elements from Coal
+    and Coal Byproducts Using Advanced Separation Technologies,
+    Honaker, R.Q., et al., DE-FE0027035
+
+    Includes the following components:
+    * Rare Earth Oxalates: "Al2(C2O4)3(s)", "Fe2(C2O4)3(s)", "Sc2(C2O4)3(s)",
+    "Y2(C2O4)3(s)", "La2(C2O4)3(s)", "Ce2(C2O4)3(s)", "Pr2(C2O4)3(s)",
+    "Nd2(C2O4)3(s)", "Sm2(C2O4)3(s)", "Gd2(C2O4)3(s)", "Dy2(C2O4)3(s)"
+
+    """
+
     CONFIG = PhysicalParameterBlock.CONFIG()
     _config_blk_build(CONFIG)
 
@@ -161,6 +178,10 @@ class _PrecipitateBlock(StateBlock):
 
 @declare_process_block_class("PrecipitateBlock", block_class=_PrecipitateBlock)
 class PrecipitateStateBlockData(StateBlockData):
+    """
+    State block for solid REE oxalate.
+    """
+
     def build(self):
         super().build()
 

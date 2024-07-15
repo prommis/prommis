1470cd9b0cae5d7da83504f6d677c63f5607d28c
Lingyan90 <lingyan.deng@keylogic.com>
2024-07-15 11:04:41 -0400

    Update crusher.py

--- src/prommis/solid_handling/crusher.py
+++ src/prommis/solid_handling/crusher.py
@@ -35,8 +35,8 @@ Crusher adds one additional constraint to calculate the work required to crush t
 
 .. math:: work_{t} = 10 * m_{t, in} * BWI * \left(\frac{1}{\sqrt{P_{t, prod, 80}}} - \frac{1}{\sqrt{P_{t, feed, 80}}}\right) 
 
-where :math: `work_{t}` is the work required to crush the particles at `t` time, 10 is an empirical value and should not be changed, :math: `m_{t,in}` is the inlet mass flow rate
-at `t` time, :math: `BWI` is the bond work index of particles, :math: `P_{t, prod, 80}` is production particle size with 80% passing the mesh at `t` time, :math: `P_{t, feed, 80}` is 
+where :math:`work_{t}` is the work required to crush the particles at `t` time, 10 is an empirical value and should not be changed, :math:`m_{t, in}` is the inlet mass flow rate
+at `t` time, :math:`BWI` is the bond work index of particles, :math:`P_{t, prod, 80}` is production particle size with 80% passing the mesh at `t` time, :math:`P_{t, feed, 80}` is 
 feed particle size with 80% passing the mesh at `t` time.
 
 Expressions
@@ -46,13 +46,13 @@ Crusher includes two expressions to calculate the size of particles that has 80%
 
 .. math:: P_{t, feed, 80} = \frac{S_{t, in, median}}{unit} * \left(-\log(1 - 0.8)\right)^{\frac{SW_{t, in}}{2}}
 
-where :math: `P_{t, feed, 80}` is the feed particle size that has 80% passing the mesh at `t` time, :math: `{S_{t, in, median}}{unit}` is the median particle size of input at `t` time and unitless. The 
-default particle size is in micrometer. The :math: `SW_{t, in}` is the particle size width of input at `t` time. 
+where :math:`P_{t, feed, 80}` is the feed particle size that has 80% passing the mesh at `t` time, :math:`{S_{t, in, median}}{unit}` is the median particle size of input at `t` time and unitless. The 
+default particle size is in micrometer. The :math:`SW_{t, in}` is the particle size width of input at `t` time. 
 
 .. math:: P_{t, prod, 80} = \frac{S_{t, out, median}}{unit} * \left(-\log(1 - 0.8)\right)^{\frac{SW_{t, out}}{2}}
 
-where :math: `P_{t, prod, 80}` is the product particle size that has 80% passing the mesh at `t` time, :math: `{S_{t, out, median}}{unit}` is the median particle size of output at `t` time and unitless. The 
-default particle size is in micrometer. The :math: `SW_{t, in}` is the particle size width of output at `t` time. 
+where :math:`P_{t, prod, 80}` is the product particle size that has 80% passing the mesh at `t` time, :math:`{S_{t, out, median}}{unit}` is the median particle size of output at `t` time and unitless. The 
+default particle size is in micrometer. The :math:`SW_{t, in}` is the particle size width of output at `t` time. 
 
 Variables
 ---------

87baa34750ee7a4db9028af7a4a10673b0d83806
Arkoprabho Dasgupta <apdg2407@gmail.com>
2024-08-12 13:04:11 -0400

    conflict resolve

--- src/prommis/solvent_extraction/sx_model_flowsheet_steady_state.py
+++ src/prommis/solvent_extraction/sx_model_flowsheet_steady_state.py
@@ -29,85 +29,80 @@ from idaes.core.util.model_diagnostics import DiagnosticsToolbox
 from idaes.core.util import to_json, from_json
 
 
-def build_model():
-    """
-    Method of building a solvent extraction model with a specified number of stages
-    and with two separate property packages for the two inlet streams.
-    This is a loading operation, so no additional argument has to be specified.
+"""
+Method of building a solvent extraction model with a specified number of stages
+and with two separate property packages for the two inlet streams.
+This is a loading operation, so no additional argument has to be specified.
 
-    """
+"""
 
-    m = ConcreteModel()
+m = ConcreteModel()
 
-    m.fs = FlowsheetBlock(dynamic=False)
+m.fs = FlowsheetBlock(dynamic=False)
 
-    m.fs.prop_o = REESolExOgParameters()
-    m.fs.leach_soln = LeachSolutionParameters()
+m.fs.prop_o = REESolExOgParameters()
+m.fs.leach_soln = LeachSolutionParameters()
 
-    number_of_stages = 3
+number_of_stages = 3
 
-    m.fs.solex = SolventExtraction(
-        number_of_finite_elements=number_of_stages,
-        dynamic=False,
-        aqueous_stream={
-            "property_package": m.fs.leach_soln,
-            "flow_direction": FlowDirection.forward,
-            "has_energy_balance": False,
-            "has_pressure_balance": False,
-        },
-        organic_stream={
-            "property_package": m.fs.prop_o,
-            "flow_direction": FlowDirection.backward,
-            "has_energy_balance": False,
-            "has_pressure_balance": False,
-        },
-    )
+m.fs.solex = SolventExtraction(
+    number_of_finite_elements=number_of_stages,
+    dynamic=False,
+    aqueous_stream={
+        "property_package": m.fs.leach_soln,
+        "flow_direction": FlowDirection.forward,
+        "has_energy_balance": False,
+        "has_pressure_balance": False,
+    },
+    organic_stream={
+        "property_package": m.fs.prop_o,
+        "flow_direction": FlowDirection.backward,
+        "has_energy_balance": False,
+        "has_pressure_balance": False,
+    },
+)
 
-    return m
+"""
+Specification of the values of the partition coefficients of the elements
+based on the values provided in the REESim file.
 
+"""
+number_of_stages = 3
+stage_number = np.arange(1, number_of_stages + 1)
 
-def set_inputs(m):
-    """
-    Specification of the values of the partition coefficients of the elements
-    based on the values provided in the REESim file.
+for s in stage_number:
+    if s == 1:
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Al"] = 5.2 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ca"] = 3 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Fe"] = 24.7 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sc"] = 99.1 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Y"] = 99.9 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "La"] = 32.4 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ce"] = 58.2 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Pr"] = 58.2 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Nd"] = 87.6 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sm"] = 99.9 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Gd"] = 69.8 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Dy"] = 96.6 / 100
+    else:
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Al"] = 4.9 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ca"] = 12.3 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Fe"] = 6.4 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sc"] = 16.7 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Y"] = 99.9 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "La"] = 23.2 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ce"] = 24.9 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Pr"] = 15.1 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Nd"] = 99.9 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sm"] = 99.9 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Gd"] = 7.6 / 100
+        m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Dy"] = 5 / 100
 
-    """
-    number_of_stages = 3
-    stage_number = np.arange(1, number_of_stages + 1)
+"""
+Fixing the inlet conditions of the two feed streams to the solvent extraction model,
+based on a case study of University of Kentucky pilot plant.
 
-    for s in stage_number:
-        if s == 1:
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Al"] = 5.2 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ca"] = 3 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Fe"] = 24.7 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sc"] = 99.1 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Y"] = 99.9 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "La"] = 32.4 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ce"] = 58.2 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Pr"] = 58.2 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Nd"] = 87.6 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sm"] = 99.9 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Gd"] = 69.8 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Dy"] = 96.6 / 100
-        else:
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Al"] = 4.9 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ca"] = 12.3 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Fe"] = 6.4 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sc"] = 16.7 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Y"] = 99.9 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "La"] = 23.2 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Ce"] = 24.9 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Pr"] = 15.1 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Nd"] = 99.9 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Sm"] = 99.9 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Gd"] = 7.6 / 100
-            m.fs.solex.partition_coefficient[s, "aqueous", "organic", "Dy"] = 5 / 100
-
-    """
-    Fixing the inlet conditions of the two feed streams to the solvent extraction model,
-    based on a case study of University of Kentucky pilot plant.
-
-    """
+"""
 
 m.fs.solex.mscontactor.aqueous_inlet_state[0].conc_mass_comp["H2O"].fix(1e6)
 m.fs.solex.mscontactor.aqueous_inlet_state[0].conc_mass_comp["H"].fix(1.755)
@@ -127,59 +122,47 @@ m.fs.solex.mscontactor.aqueous_inlet_state[0].conc_mass_comp["Gd"].fix(0.2584)
 m.fs.solex.mscontactor.aqueous_inlet_state[0].conc_mass_comp["Dy"].fix(0.047)
 m.fs.solex.mscontactor.aqueous_inlet_state[0].conc_mass_comp["Cl"].fix(1e-8)
 
-    m.fs.solex.mscontactor.aqueous_inlet_state[0].flow_vol.fix(62.01)
+m.fs.solex.mscontactor.aqueous_inlet_state[0].flow_vol.fix(62.01)
 
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Al"].fix(1.267e-5)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Ca"].fix(2.684e-5)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Fe"].fix(2.873e-6)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Sc"].fix(1.734)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Y"].fix(2.179e-5)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["La"].fix(0.000105)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Ce"].fix(0.00031)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Pr"].fix(3.711e-5)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Nd"].fix(0.000165)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Sm"].fix(1.701e-5)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Gd"].fix(3.357e-5)
-    m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Dy"].fix(8.008e-6)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Al"].fix(1.267e-5)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Ca"].fix(2.684e-5)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Fe"].fix(2.873e-6)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Sc"].fix(1.734)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Y"].fix(2.179e-5)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["La"].fix(0.000105)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Ce"].fix(0.00031)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Pr"].fix(3.711e-5)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Nd"].fix(0.000165)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Sm"].fix(1.701e-5)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Gd"].fix(3.357e-5)
+m.fs.solex.mscontactor.organic_inlet_state[0].conc_mass_comp["Dy"].fix(8.008e-6)
 
-    m.fs.solex.mscontactor.organic_inlet_state[0].flow_vol.fix(62.01)
+m.fs.solex.mscontactor.organic_inlet_state[0].flow_vol.fix(62.01)
 
+"""
+Initialization of the model, which gives a good starting point.
 
-if __name__ == "__main__":
+"""
 
-    number_of_stages = 3
+initializer = SolventExtractionInitializer()
+initializer.initialize(m.fs.solex)
 
-    m = build_model()
-    set_inputs(m)
+""" 
+Solution of the model and display of the final results.
 
-    """
-    Initialization of the model, which gives a good starting point.
+"""
 
-    """
+solver = SolverFactory("ipopt")
+solver.options["bound_push"] = 1e-8
+solver.options["mu_init"] = 1e-8
+solver.solve(m, tee=True)
 
-    initializer = SolventExtractionInitializer()
-    try:
-        initializer.initialize(m.fs.solex)
-    except:
-        pass
+to_json(m, fname="ex.json.gz", gz=True, human_read=True)
 
-    """ 
-    Solution of the model and display of the final results.
+# Final organic outlet display
+m.fs.solex.mscontactor.organic[0, 1].conc_mass_comp.display()
+m.fs.solex.mscontactor.organic[0, 1].conc_mol_comp.display()
 
-    """
-
-    solver = SolverFactory("ipopt")
-    solver.options["bound_push"] = 1e-8
-    solver.options["mu_init"] = 1e-8
-    solver.solve(m, tee=True)
-
-    to_json(m, fname="ex.json.gz", gz=True, human_read=True)
-
-
-    # Final organic outlet display
-    m.fs.solex.mscontactor.organic[0, 1].conc_mass_comp.display()
-    m.fs.solex.mscontactor.organic[0, 1].conc_mol_comp.display()
-
-    # Final aqueous outlets display
-    m.fs.solex.mscontactor.aqueous[0, number_of_stages].conc_mass_comp.display()
-    m.fs.solex.mscontactor.aqueous[0, number_of_stages].conc_mol_comp.display()
+# Final aqueous outlets display
+m.fs.solex.mscontactor.aqueous[0, number_of_stages].conc_mass_comp.display()
+m.fs.solex.mscontactor.aqueous[0, number_of_stages].conc_mol_comp.display()

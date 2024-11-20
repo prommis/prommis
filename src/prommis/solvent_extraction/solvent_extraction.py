c9210e8c4d5bdc5b4b42bc4c9aa0cbc2fcabe825
Arkoprabho Dasgupta <apdg2407@gmail.com>
2024-11-20 11:32:35 -0500

    idaes header removed

--- src/prommis/solvent_extraction/solvent_extraction.py
+++ src/prommis/solvent_extraction/solvent_extraction.py
@@ -4,18 +4,6 @@
 # University of California, through Lawrence Berkeley National Laboratory, et al. All rights reserved.
 # Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license information.
 #####################################################################################################
-#################################################################################
-# The Institute for the Design of Advanced Energy Systems Integrated Platform
-# Framework (IDAES IP) was produced under the DOE Institute for the
-# Design of Advanced Energy Systems (IDAES).
-#
-# Copyright (c) 2018-2023 by the software owners: The Regents of the
-# University of California, through Lawrence Berkeley National Laboratory,
-# National Technology & Engineering Solutions of Sandia, LLC, Carnegie Mellon
-# University, West Virginia University Research Corporation, et al.
-# All rights reserved.  Please see the files COPYRIGHT.md and LICENSE.md
-# for full copyright and license information.
-#################################################################################
 
 """
 Solvent Extraction Model

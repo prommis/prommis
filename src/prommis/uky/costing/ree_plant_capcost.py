c379d4fb2f4d23c07ab5016a421e85ea94de7f46
Brandon Paul <bpaul@keylogic.com>
2025-01-22 15:24:31 -0800

    run isort

--- src/prommis/uky/costing/ree_plant_capcost.py
+++ src/prommis/uky/costing/ree_plant_capcost.py
@@ -33,10 +33,9 @@ from pyomo.common.config import ConfigValue, ListOf
 from pyomo.common.dependencies import attempt_import
 from pyomo.core.base.expression import ScalarExpression
 from pyomo.core.base.units_container import InconsistentUnitsError, UnitsError
-from pyomo.environ import ConcreteModel, Expression, Param, Reference, Var
+from pyomo.environ import ConcreteModel, Expression, Param, Reference, Var, log10
 from pyomo.environ import units as pyunits
 from pyomo.environ import value
-from pyomo.environ import log10
 from pyomo.util.calc_var_value import calculate_variable_from_constraint
 
 import idaes.core.util.scaling as iscale

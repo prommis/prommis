daa5a7b47fcc429763cdb621157f56e76696e16d
Chris Laliwala <claliwal@andrew.cmu.edu>
2025-02-04 11:20:29 -0500

    switched if statement for assertion to check if dofs are 0.

--- src/prommis/examples/cmi_process_flowsheet/tests/test_cmi_process_flowsheet.py
+++ src/prommis/examples/cmi_process_flowsheet/tests/test_cmi_process_flowsheet.py
@@ -118,8 +118,7 @@ def test_build_flowsheet(system_frame):
 @pytest.mark.solver
 def test_degrees_of_freedom(system_frame):
     m = system_frame
-    if degrees_of_freedom(m) != 0:
-        raise AssertionError("The degrees of freedom are not equal to 0.")
+    assert (degrees_of_freedom(m) == 0)
 
 
 @pytest.mark.component

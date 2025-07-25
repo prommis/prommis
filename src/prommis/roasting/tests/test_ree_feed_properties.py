e0b26b5ffaba8e483ed63ac176cea9e56463a504
Jinliang Ma <jinliang.ma@netl.doe.gov>
2025-07-25 14:27:14 -0400

    Revised test_ree_feed_properties.py to improve code coverage

--- src/prommis/roasting/tests/test_ree_feed_properties.py
+++ src/prommis/roasting/tests/test_ree_feed_properties.py
@@ -6,12 +6,14 @@
 #####################################################################################################
 from pyomo.environ import ConcreteModel, Constraint, Param, Var, value
 
-from idaes.core import FlowsheetBlock
+from idaes.core import FlowsheetBlock, MaterialFlowBasis
+from idaes.core.solvers import get_solver
 
 import pytest
 
 from prommis.roasting.ree_feed_properties import ReeFeedParameters
 
+solver = get_solver()
 
 @pytest.fixture
 def model():
@@ -70,7 +72,9 @@ def test_parameters(model):
         assert k in model.fs.feed_prop.cp1_comp
 
     assert isinstance(model.fs.feed_prop.dens_mass, Param)
+    assert isinstance(model.fs.feed_prop.temperature_ref, Param)
     assert value(model.fs.feed_prop.dens_mass) == pytest.approx(2000, rel=1e-8)
+    assert value(model.fs.feed_prop.temperature_ref) == pytest.approx(298.15, rel=1e-8)
 
 
 @pytest.mark.unit
@@ -104,6 +108,8 @@ def test_fix_state(model):
 
     model.fs.state.fix_initialization_states()
 
+    solver.solve(model)
+
     assert model.fs.state[0].flow_mass.fixed
     assert model.fs.state[0].temperature.fixed
     for j in model.fs.feed_prop.component_list:
@@ -113,3 +119,8 @@ def test_fix_state(model):
         assert model.fs.state[0].enth_mol_comp_constraint[j].active
     assert model.fs.state[0].enth_mol_constraint.active
     assert model.fs.state[0].enth_mass_constraint.active
+    assert value(model.fs.state[0].get_material_density_terms('solid', 'Ree2X')) == pytest.approx(2000, rel=1e-8)
+    assert value(model.fs.state[0].get_energy_density_terms('solid')) == pytest.approx(34158356078.71398, rel=1e-8)
+    assert model.fs.state[0].get_material_flow_basis()==MaterialFlowBasis.molar
+    assert value(model.fs.state[0].get_material_flow_terms('solid', 'Ree2X')) == pytest.approx(0.8064613684842962, rel=1e-8)
+    assert value(model.fs.state[0].get_enthalpy_flow_terms('solid')) == pytest.approx(170791780.39356998, rel=1e-8)

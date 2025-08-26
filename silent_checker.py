ec73838e0348759a97998f53b5540c94a1f1e9ef
Sufi Kaur <sufikaur@lbl.gov>
2025-08-26 14:41:01 -0700

    formatting

--- silent_checker.py
+++ silent_checker.py
@@ -2,19 +2,20 @@ import astroid
 from astroid import nodes
 from typing import TYPE_CHECKING
 
-from pylint.checkers import BaseChecker, IAstroidChecker  
+from pylint.checkers import BaseChecker, IAstroidChecker
 
 if TYPE_CHECKING:
     from pylint.lint import PyLinter
 
+
 class SilentExceptionChecker(BaseChecker):
     __implements__ = IAstroidChecker
-    
+
     name = "silent-exception"
     msgs = {
         "W9006": (
             "Exception without handling, only pass, detected",
-            'silent-exception-handling',
+            "silent-exception-handling",
             "Exception blocks should not have a bare pass",
         )
     }
@@ -26,20 +27,23 @@ class SilentExceptionChecker(BaseChecker):
     def _visit_try_except(self, node):
         for handler in node.handlers:
             if self._is_silent_exception(handler):
-                self.add_message('silent-exception-handling', node=handler)
-        
+                self.add_message("silent-exception-handling", node=handler)
+
     def _is_silent_exception(self, handler):
         # checks bare except
         if not handler.type:  # bare except:
             return len(handler.body) == 1 and isinstance(handler.body[0], astroid.Pass)
-        
+
         # checks only pass after except
-        if (isinstance(handler.type, astroid.Name) and 
-            handler.type.name in ('Exception', 'BaseException') and
-            len(handler.body) == 1 and 
-            isinstance(handler.body[0], astroid.Pass)):
+        if (
+            isinstance(handler.type, astroid.Name)
+            and handler.type.name in ("Exception", "BaseException")
+            and len(handler.body) == 1
+            and isinstance(handler.body[0], astroid.Pass)
+        ):
             return True
         return False
+
 
 def register(linter: "PyLinter") -> None:
     """This required method auto registers the checker during initialization."""

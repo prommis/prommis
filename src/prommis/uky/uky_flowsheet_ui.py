142af79602535920614a55ae321d0cf2108024b3
MarcusHolly <marcus.holly@keylogic.com>
2025-08-27 08:53:32 -0400

    Minor improvements to UKy UI file

--- src/prommis/uky/uky_flowsheet_ui.py
+++ src/prommis/uky/uky_flowsheet_ui.py
@@ -167,6 +167,7 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
             obj=lsf.mass_frac_comp[0, compound],
             name=f"Leach solid feed {compound}",
             description=f"Leach solid feed {compound} fractional composition",
+            display_units="fraction",
             rounding=3,
             is_input=True,
             is_output=False,
@@ -198,7 +199,7 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
     )
     exports.add(
         obj=rst.gas_inlet.pressure[0],
-        name="Gas inlet temperature",
+        name="Gas inlet pressure",
         rounding=2,
         ui_units=pyo.units.Pa,
         display_units="Pa",
@@ -241,7 +242,7 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
 
     # Export the leach solid outputs, which includes overall mass flow,
     # and mass fraction of oxides and inerts.
-    category = "solids"
+    category = "leaching solid outlet"
     leach = flowsheet.leach
     exports.add(
         obj=leach.solid_outlet.flow_mass[0],
@@ -249,7 +250,7 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
         rounding=4,
         ui_units=pyo.units.kg / pyo.units.hour,
         display_units="kg/hr",
-        description=f"solid flow mass",
+        description=f"Leaching solid flow mass",
         is_input=False,
         is_output=True,
         output_category=category,
@@ -273,15 +274,15 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
         description=f"leaching solid mass fraction of inert components outlet",
         is_input=False,
         is_output=True,
-        output_category="solid outlet",
+        output_category=category,
     )
 
     # Export leach liquid outputs, which includes the liquid flow
     # and liquid mass compositions
-    category = "leaching"
+    category = "Leaching liquid outlet"
     exports.add(
         obj=leach.liquid_outlet.flow_vol[0],
-        name=f"liquid flow volume",
+        name=f"Leaching liquid flow volume",
         ui_units=pyo.units.l / pyo.units.hour,
         display_units="l/h",
         rounding=4,
@@ -313,7 +314,7 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
         "cleaner_load",
         "cleaner_strip",
     }:
-        category = f"solex {stype}"
+        category = f"Solvent extraction {stype}"
         block = getattr(flowsheet, f"solex_{stype}")
         for ltype in {"organic", "aqueous"}:
             if stype == "rougher" and ltype == "aqueous":
@@ -343,11 +344,11 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
     # Export the outputs for the precipitator, including overall flow
     # as well as concentration mass composition for chemical components
     # and precipitate components.
-    category = "precipitator"
+    category = "precipitator aqueous outlet"
     precipitator = flowsheet.precipitator
     exports.add(
         obj=precipitator.cv_aqueous.properties_out[0].flow_vol,
-        name=f"precipitator aqueous out",
+        name=f"precipitator aqueous outlet flow rate",
         ui_units=pyo.units.l / pyo.units.hour,
         display_units="liters/hour",
         rounding=4,
@@ -370,6 +371,7 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
             is_output=True,
             output_category=category,
         )
+    category = "precipitator solid outlet"
     exports.add(
         obj=precipitator.precipitate_outlet.temperature[0],
         name="precipitator outlet temperature",
@@ -398,7 +400,7 @@ def export_variables(flowsheet=None, exports=None, build_options=None, **kwargs)
 
     # Export the outputs for the roaster, including product mass flow
     # and molar flow rates for the oxides in the product stream.
-    category = "roaster"
+    category = "Roaster product"
     roaster = flowsheet.roaster
     name = f"roaster product mass flow of total oxides"
     obj = roaster.flow_mass_product[0]

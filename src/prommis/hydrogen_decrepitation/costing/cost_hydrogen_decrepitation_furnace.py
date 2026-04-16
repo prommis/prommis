34523b6913f82f4edb608c74a064d0118412b60d
Akintomiwa Ojo <akintomiwaoluwapelumiojo@gmail.com>
2026-04-16 11:46:36 -0400

    restructure HD costing code.

--- src/prommis/hydrogen_decrepitation/costing/cost_hydrogen_decrepitation_furnace.py
+++ src/prommis/hydrogen_decrepitation/costing/cost_hydrogen_decrepitation_furnace.py
@@ -1,6 +1,6 @@
 #####################################################################################################
-# “PrOMMiS” was produced under the DOE Process Optimization and Modeling for Minerals Sustainability
-# (“PrOMMiS”) initiative, and is copyright (c) 2023-2025 by the software owners: The Regents of the
+# "PrOMMiS" was produced under the DOE Process Optimization and Modeling for Minerals Sustainability
+# ("PrOMMiS") initiative, and is copyright (c) 2023-2025 by the software owners: The Regents of the
 # University of California, through Lawrence Berkeley National Laboratory, et al. All rights reserved.
 # Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license information.
 #####################################################################################################
@@ -61,22 +61,109 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
         # Set a base period for all operating costs
         self.base_period = pyunits.year
 
+        self.price_insulation1 = Param(
+            mutable=True,
+            units=self.base_currency,
+            doc="Price per unit of insulation material 1, as specified by the vendor",
+        )
+        self.price_insulation1.set_value(183.81 * pyunits.USD_Jan_2024)
+
+        self.labor_rate = Param(
+            mutable=True,
+            units=self.base_currency / pyunits.hr,
+            doc="Hourly rate of plant operators in project dollar year",
+        )
+        self.labor_rate.set_value(75 * pyunits.USD_Jan_2024 / pyunits.hr)
+
+        self.price_metal1 = Param(
+            mutable=True,
+            units=self.base_currency / pyunits.kg,
+            doc="Price of stainless steel 304 (in $/kg); "
+            "from https://mepsinternational.com/gb/en/products/world-stainless-steel-prices "
+            "as of 01/01/2024",
+        )
+        self.price_metal1.set_value(3.14 * pyunits.USD_Jan_2024 / pyunits.kg)
+
+        self.price_insulation2 = Param(
+            mutable=True,
+            units=self.base_currency,
+            doc="Price per unit of insulation material 2, as specified by the vendor",
+        )
+        self.price_insulation2.set_value(47.00 * pyunits.USD_Jan_2024)
+
+        self.price_metal2 = Param(
+            mutable=True,
+            units=self.base_currency / pyunits.kg,
+            doc="Price of carbon steel (in $/kg); "
+            "from https://mepsinternational.com/gb/en/products/north-america-steel-prices "
+            "as of 01/01/2024",
+        )
+        self.price_metal2.set_value(1.50 * pyunits.USD_Jan_2024 / pyunits.kg)
+
+        self.efficiency = Param(
+            mutable=True,
+            units=pyunits.dimensionless,
+            doc="Power usage efficiency",
+        )
+        self.efficiency.set_value(0.95 * pyunits.dimensionless)
+
+        self.hours_per_shift = Param(
+            mutable=True,
+            units=pyunits.hr,
+            doc="Number of hours per shift",
+        )
+        self.hours_per_shift.set_value(8 * pyunits.hr)
+
+        self.shifts_per_day = Param(
+            mutable=True,
+            units=1 / pyunits.day,
+            doc="Number of shifts per day",
+        )
+        self.shifts_per_day.set_value(3 * (pyunits.day) ** (-1))
+
+        self.operating_days_per_year = Param(
+            mutable=True,
+            units=pyunits.day / pyunits.year,
+            doc="Number of operating days per year",
+        )
+        self.operating_days_per_year.set_value(336 * pyunits.day / pyunits.year)
+
+        self.utility_rate = Param(
+            mutable=True,
+            units=self.base_currency / (pyunits.kW * pyunits.hr),
+            doc="Unit rate for energy consumption; "
+            "from https://www.eia.gov/electricity/monthly/epm_table_grapher.php?t=epmt_5_03 "
+            "as of 01/01/2024",
+        )
+        self.utility_rate.set_value(
+            0.081 * pyunits.USD_Jan_2024 / (pyunits.kW * pyunits.hr)
+        )
+
+        self.temperature_controller_price = Param(
+            mutable=True,
+            units=self.base_currency,
+            doc="Unit cost of the temperature controller; "
+            "from https://www.iothrifty.com/products/n480d-low-cost-pid-temperature-controller"
+            "?variant=42613548187885 as of 01/01/2024",
+        )
+        self.temperature_controller_price.set_value(129.00 * pyunits.USD_Jan_2024)
+
+        self.engineering_and_drafting = Param(
+            mutable=True,
+            units=self.base_currency,
+            doc="Flat fee for design, drafting, analysis, consultation, and project management",
+        )
+        self.engineering_and_drafting.set_value(1000 * pyunits.USD_Jan_2024)
+
+        self.CE_index_year = Param(
+            initialize="Jan_2024",
+            mutable=True,
+            doc="Base year to convert costing values to, e.g. 'Jan_2024'",
+        )
+
     def cost_hydrogen_decrepitation_furnace(
         blk,
-        price_insulation1=183.81 * pyunits.USD_Jan_2024,
-        price_metal1=3.14 * pyunits.USD_Jan_2024 / pyunits.kg,
-        price_insulation2=47.00 * pyunits.USD_Jan_2024,
-        price_metal2=1.50 * pyunits.USD_Jan_2024 / pyunits.kg,
-        hours_per_shift=8 * pyunits.hr,
-        shifts_per_day=3 * (pyunits.day) ** (-1),
-        operating_days_per_year=336 * pyunits.day / pyunits.year,
-        efficiency=0.95 * pyunits.dimensionless,
-        utility_rate=0.081 * pyunits.USD_Jan_2024 / (pyunits.kW * pyunits.hr),
         heating_mode=0,
-        labor_rate=75 * pyunits.USD_Jan_2024 / pyunits.hr,
-        temperature_controller_price=129.00 * pyunits.USD_Jan_2024,
-        engineering_and_drafting=1000 * pyunits.USD_Jan_2024,
-        CE_index_year="Jan_2024",
     ):
         """
         Cost Estimation of an Hydrogen Decrepitation Furnace
@@ -86,28 +173,7 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
         Args:
         blk: A unit-level costing block where costing variables and
              constraints can be added to.
-        price_insulation1: The price of a unit of the first insulation material,
-                           as specified by the vendor.
-        price_metal1: Price of metal material 1 in USD/kg (default price is for stainless steel 304)
-                      from https://mepsinternational.com/gb/en/products/world-stainless-steel-prices as of 01/01/2024
-        price_insulation2: The price of a unit of the second insulation material,
-                           as specified by the vendor.
-        price_metal2: Price of metal material 2 in USD/kg (default price is for carbon steel)
-                      from https://mepsinternational.com/gb/en/products/north-america-steel-prices as of 01/01/2024
-        hours_per_shift: Number of hours per shift
-        shifts_per_day: Number of shifts per day
-        operating_days_per_year: Number of operating days per year
-        efficiency: Power usage efficiency
-        utility_rate: Unit rate for energy consumption
-                        from https://www.eia.gov/electricity/monthly/epm_table_grapher.php?t=epmt_5_03 as of 01/01/2024
         heating_mode: mode of operation of the heating system. 0 - electricity, 1 - gas-fired
-        labor_rate: Hourly rate of plant operators in project dollar year;
-        temperature_controller_price: Unit cost of the temperature controller
-                        from https://www.iothrifty.com/products/n480d-low-cost-pid-temperature-controller?variant=42613548187885&gad_source=1&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA832gIq8ZE3oqLv_OZKHllF8qHXsZ8l48v-urT1a3OCoxQifOp6E1YaAjKJEALw_wcB
-                        as of 01/01/2024
-        engineering_and_drafting: A flat fee charged for design, drafting, analysis, consultation,
-                        and project management.
-        CE_index_year: Base year to convert costing values to, e.g. "Jan_2024".
         """
         if not isinstance(blk.parent_block(), REPMHydrogenDecrepitationFurnace):
             raise TypeError(
@@ -125,94 +191,54 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
 
         # Material costs
 
-        blk.price_insulation1 = Param(
-            units=blk.costing_package.base_currency, mutable=True
-        )
-        blk.price_insulation1.set_value(price_insulation1)
-
         @blk.Expression(doc="Total cost of insulation material 1")
         def material_cost_insulation1(b):
-            return b.parent_block().quantity_insulation1 * b.price_insulation1
-
-        blk.labor_rate = Param(
-            units=blk.costing_package.base_currency / pyunits.hr, mutable=True
-        )
-        blk.labor_rate.set_value(labor_rate)
-
-        blk.price_metal1 = Param(
-            mutable=True,
-            units=blk.costing_package.base_currency / pyunits.kg,
-            doc="Price of stainless steel 304 (in $/kg)",
-        )
-        blk.price_metal1.set_value(price_metal1)
+            return (
+                b.parent_block().quantity_insulation1
+                * b.costing_package.price_insulation1
+            )
 
         @blk.Expression(doc="Cost of metal material 1")
         def material_cost_metal1(b):
             return (
                 pyunits.convert(b.parent_block().weight_metal1, to_units=pyunits.kg)
-                * b.price_metal1
+                * b.costing_package.price_metal1
             )
-
-        blk.price_insulation2 = Param(
-            units=blk.costing_package.base_currency, mutable=True
-        )
-        blk.price_insulation2.set_value(price_insulation2)
 
         @blk.Expression(doc="Material cost for insulation material 2")
         def material_cost_insulation2(b):
-            return b.parent_block().quantity_insulation2 * b.price_insulation2
-
-        blk.price_metal2 = Param(
-            mutable=True,
-            units=blk.costing_package.base_currency / pyunits.kg,
-            doc="Price of carbon steel (in $/kg)",
-        )
-        blk.price_metal2.set_value(price_metal2)
+            return (
+                b.parent_block().quantity_insulation2
+                * b.costing_package.price_insulation2
+            )
 
         @blk.Expression(doc="Cost of metal material 2")
         def material_cost_metal2(b):
             return (
                 pyunits.convert(b.parent_block().weight_metal2, to_units=pyunits.kg)
-                * b.price_metal2
+                * b.costing_package.price_metal2
             )
 
         # Energy costs
-
-        blk.efficiency = Param(units=pyunits.dimensionless, mutable=True)
-        blk.efficiency.set_value(efficiency)
 
         @blk.Expression(blk.flowsheet().config.time, doc="Power rating of the furnace")
         def furnace_power_rating(b, t):
             return (
                 b.parent_block().heat_furnace_material
                 + b.parent_block().heat_sample_material[t]
-            ) / (b.parent_block().ramp_up_time * b.efficiency)
-
-        blk.hours_per_shift = Param(units=pyunits.hr, mutable=True)
-        blk.hours_per_shift.set_value(hours_per_shift)
-        blk.shifts_per_day = Param(units=1 / pyunits.day, mutable=True)
-        blk.shifts_per_day.set_value(shifts_per_day)
-        blk.operating_days_per_year = Param(
-            units=pyunits.day / pyunits.year,
-            mutable=True,
-        )
-        blk.operating_days_per_year.set_value(operating_days_per_year)
+            ) / (b.parent_block().ramp_up_time * b.costing_package.efficiency)
 
         @blk.Expression(doc="Number of batches processed annually")
         def total_runs(b):
             return (
-                b.hours_per_shift * b.shifts_per_day * b.operating_days_per_year
+                b.costing_package.hours_per_shift
+                * b.costing_package.shifts_per_day
+                * b.costing_package.operating_days_per_year
             ) / b.parent_block().processing_time
 
         @blk.Expression(doc="Heat duty required annually")
         def annual_heat_duty(b):
             return b.parent_block().total_heat_duty[0] * b.total_runs
-
-        blk.utility_rate = Param(
-            units=blk.costing_package.base_currency / (pyunits.kW * pyunits.hr),
-            mutable=True,
-        )
-        blk.utility_rate.set_value(utility_rate)
 
         blk.OPEX = Var(
             within=PositiveReals,
@@ -224,11 +250,11 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
         @blk.Constraint()
         def operating_cost_eq(b):
             return b.OPEX == pyunits.convert(
-                (b.parent_block().total_heat_duty[0] / b.efficiency)
-                * b.hours_per_shift
-                * b.shifts_per_day
-                * b.operating_days_per_year
-                * b.utility_rate,
+                (b.parent_block().total_heat_duty[0] / b.costing_package.efficiency)
+                * b.costing_package.hours_per_shift
+                * b.costing_package.shifts_per_day
+                * b.costing_package.operating_days_per_year
+                * b.costing_package.utility_rate,
                 to_units=blk.costing_package.base_currency
                 / blk.costing_package.base_period,
             )
@@ -246,8 +272,7 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
             # https://hvacdirect.com/goodman-5-kilowatt-16-200-btu-package-unit-heater-coil-hkp-05c.html
             # https://www.hvacpartsshop.com/l99h008-heating-element-5kw/
             # https://www.alpinehomeair.com/product/air-handlers-electric-furnaces/heater-coils/blueridge/bmahk20
-            # https://kaleidoroasters.com/products/kaleido-m10-heating-element?variant=47908078584088
-            # https://surpluscityliquidators.com/products/36-kw-electric-heat-kit-7510-ton-air-handlers-208230603.html?srsltid=AfmBOooYvdG8SXAoZufjtif3FUDxYv5JEP180WmuctSv8827yoOfNOWQ0EI
+            # https://surpluscityliquidators.com/products/36-kw-electric-heat-kit-7510-ton-air-handlers-208230603.html
             # as of 01/01/2024
             def cost_heating_device(b, t):
                 return pyunits.convert(
@@ -267,10 +292,10 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
             @blk.Expression(doc="Cost of heating device")
             # Approximate cost correlation based on gas burner price from
             # https://www.combustion-plus.com/product/maxon-industrial-burner-ople40sunss11dcs-2/
-            # https://horizonpfm.com/midco-ec300-economite-gas-burner-300-000-btu-hr/ec300/?setCurrencyId=1&sku=EC300&gad_source=1&gclid=Cj0KCQjw6oi4BhD1ARIsAL6pox2As5QlWrze2Yvyp0BjCFlZ1ydT5OwNJHnM7vTYDpnW79R8nVFP3FIaAsTsEALw_wcB
-            # https://burnerparts.com/eclipse-airheat-burner-ah0200-v2-in-stock-ready-to-ship.html?gad_source=4&gclid=Cj0KCQjw6oi4BhD1ARIsAL6pox25C7_IYEYxEoMxizuLW3mVaGdK8acUwlN8JOoeEjhi2zr7nh8BzA0aAvioEALw_wcB
-            # https://combustionsupplies.com/products/eclipse-thermjet-tj0100-high-velocity-burner-new-1?variant=43165518463202&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&gad_source=1&gclid=Cj0KCQjw6oi4BhD1ARIsAL6pox0YMnAnjvsiEjkvVHJwGKSU02d10VVwEzjD-bkaJ7PZIdnmmfbXP7IaAvv9EALw_wcB
-            # https://oswaldsupply.com/products/economite-ec300-gas-conversion-burner-min-90-000-max-300-000-btu-hr?variant=33944221450377&kw=EC300&c=Shopping&utm_source=google&utm_medium=cpc&utm_campaign=PerformanceMax-SmartShopping&kw=&ad=&matchtype=&adposition=&c=PerformanceMaxUS&gad_source=1&gclid=Cj0KCQjw6oi4BhD1ARIsAL6pox2c_z-E247Mledh5o9CpHrsJYBodWY8NB5eGzHcnytENYiLSLvCwvQaAtOjEALw_wcB
+            # https://horizonpfm.com/midco-ec300-economite-gas-burner-300-000-btu-hr/ec300/
+            # https://burnerparts.com/eclipse-airheat-burner-ah0200-v2-in-stock-ready-to-ship.html
+            # https://combustionsupplies.com/products/eclipse-thermjet-tj0100-high-velocity-burner-new-1?variant=43165518463202
+            # https://oswaldsupply.com/products/economite-ec300-gas-conversion-burner-min-90-000-max-300-000-btu-hr?variant=33944221450377
             def cost_heating_device(b, t):
                 return pyunits.convert(
                     (
@@ -293,28 +318,17 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
         @blk.Expression(doc="Total labor cost")
         def labor_cost(b):
             return smooth_max(
-                3 * pyunits.hr * b.labor_rate,
+                3 * pyunits.hr * b.costing_package.labor_rate,
                 (
                     (
                         6.79
                         * (pyunits.hr / (pyunits.m**2))
                         * b.parent_block().furnace_external_surface_area
                     )
-                    * b.labor_rate
+                    * b.costing_package.labor_rate
                 ),
                 eps=b.eps2,
             )
-
-        blk.temperature_controller_price = Param(
-            units=blk.costing_package.base_currency,
-            mutable=True,
-        )
-        blk.temperature_controller_price.set_value(temperature_controller_price)
-
-        blk.engineering_and_drafting = Param(
-            units=blk.costing_package.base_currency, mutable=True
-        )
-        blk.engineering_and_drafting.set_value(engineering_and_drafting)
 
         @blk.Expression(doc="Overhead Cost")
         def overhead_cost(b, t):
@@ -323,10 +337,10 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
                 + b.material_cost_metal1
                 + b.material_cost_insulation2
                 + b.material_cost_metal2
-                + b.temperature_controller_price
+                + b.costing_package.temperature_controller_price
                 + b.cost_heating_device
                 + b.labor_cost
-                + b.engineering_and_drafting
+                + b.costing_package.engineering_and_drafting
             )
 
         blk.base_cost_per_unit = Var(
@@ -343,10 +357,10 @@ class HydrogenDecrepitationCostingData(FlowsheetCostingBlockData):
                 + b.material_cost_metal1
                 + b.material_cost_insulation2
                 + b.material_cost_metal2
-                + b.temperature_controller_price
+                + b.costing_package.temperature_controller_price
                 + b.cost_heating_device
                 + b.labor_cost
-                + b.engineering_and_drafting
+                + b.costing_package.engineering_and_drafting
                 + b.overhead_cost
             )
 

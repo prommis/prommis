29bb7fe33162de4154589eeff2d2afa7429b7445
Carolina Tristan <ctristan@purdue.edu>
2026-09-14 16:17:53 -0400

    Update src/prommis/precipitate/precipitate_liquid_properties.py
    Co-authored-by: dallan-keylogic <88728506+dallan-keylogic@users.noreply.github.com>

--- src/prommis/precipitate/precipitate_liquid_properties.py
+++ src/prommis/precipitate/precipitate_liquid_properties.py
@@ -215,7 +215,6 @@ class AqueousStateBlockkData(StateBlockData):
         )
         self.flow_mass_comp = Var(
             self.params.dissolved_elements,
-            # units=units.kg / units.s,
             units=units.kg / units.hour,
             initialize=1,
             bounds=(1e-20, None),
